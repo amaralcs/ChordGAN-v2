468480b2aac6a07983840aded84b45944380fc66
amaralcs <amaralcscgp@gmail.com>
2022-07-30 11:41:39 +0100

    Model `call` method supports custom sampling rate

--- src/model.py
+++ src/model.py
@@ -236,7 +236,7 @@ class ChordGAN(Model):
 
         return {"d_loss": d_loss, "g_loss": g_loss}
 
-    def call(self, chroma, low_note=24, high_note=102):
+    def call(self, chroma, low_note=24, high_note=102, **kwargs):
         """Converts a song given its chroma. The chroma needs to have been correctly reshaped
         as per the preprocessing function.
 
@@ -248,6 +248,8 @@ class ChordGAN(Model):
             Index of lowest note to keep.
         high_note : int
             Index of highest note to keep.
+        kwargs 
+            Arguments to be passed to `piano_roll_to_pretty_midi`.
         """
         # The generator returns a tensor of shape
         converted_song = self.generator(chroma).numpy()
@@ -257,7 +259,7 @@ class ChordGAN(Model):
         # set all non-zero velocities to 127
         piano_roll_thresh = (piano_roll >= 0.5) * 127
 
-        return piano_roll_to_pretty_midi(piano_roll_thresh, fs=16)
+        return piano_roll_to_pretty_midi(piano_roll_thresh, **kwargs)
 
     def summary(self):
         self.generator.summary()
